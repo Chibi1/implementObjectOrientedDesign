5b99e7b4db5abc7d7e2e2c835325e9f711eb4404
Chibi1 <chibueze.e.njoku@gmail.com>
2019-06-02 21:05:43 +1000

    implmented function using equence diagram

--- SelfDrivingCarAfterFields/src/model/AutoDriver.java
+++ SelfDrivingCarAfterFields/src/model/AutoDriver.java
@@ -17,7 +17,11 @@ public class AutoDriver {
     // MODIFIES: SpeedRegulator
     // EFFECTS: for seconds iterations, slows down if too close to object in front
     public void driveForward(int seconds){
-
+        for (int i = 0; i < seconds; i++) {
+            if (frontSensor.tooClose()) {
+                speedRegulator.slowDown();
+            }
+        }
     }
 
 
